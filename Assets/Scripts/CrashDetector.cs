227d9190a2fa82084a8bdf75dee8c4076a226fd2
Noel-Teens <teeniejoshua@gmail.com>
2025-07-16 13:08:06 +0530

    soundfx added

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -5,12 +5,14 @@ public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float invokeTime = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", invokeTime);
         }
     }
